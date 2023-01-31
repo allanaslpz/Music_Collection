2d4a011bfdcdd50c93ab5fe3f2cc4bc351abd92d
alopez12-nc <122580528+alopez12-nc@users.noreply.github.com>
2023-01-31 10:09:45 -0500

    mvc music collection
    First push

---
+++ MVC_Music_Solution-Part4/MVC_Music/Data/ExtraMigration.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVC_Music.Data
+{
+    public static class ExtraMigration
+    {
+        public static void Steps(MigrationBuilder migrationBuilder)
+        {
+            //Triggers for Musician
+            migrationBuilder.Sql(
+                @"
+                    CREATE TRIGGER SetMusicianTimestampOnUpdate
+                    AFTER UPDATE ON Musicians
+                    BEGIN
+                        UPDATE Musicians
+                        SET RowVersion = randomblob(8)
+                        WHERE rowid = NEW.rowid;
+                    END
+                ");
+            migrationBuilder.Sql(
+                @"
+                    CREATE TRIGGER SetMusicianTimestampOnInsert
+                    AFTER INSERT ON Musicians
+                    BEGIN
+                        UPDATE Musicians
+                        SET RowVersion = randomblob(8)
+                        WHERE rowid = NEW.rowid;
+                    END
+                ");
+
+            //Triggers for Album
+            migrationBuilder.Sql(
+                @"
+                    CREATE TRIGGER SetAlbumTimestampOnUpdate
+                    AFTER UPDATE ON Albums
+                    BEGIN
+                        UPDATE Albums
+                        SET RowVersion = randomblob(8)
+                        WHERE rowid = NEW.rowid;
+                    END
+                ");
+            migrationBuilder.Sql(
+                @"
+                    CREATE TRIGGER SetAlbumTimestampOnInsert
+                    AFTER INSERT ON Albums
+                    BEGIN
+                        UPDATE Albums
+                        SET RowVersion = randomblob(8)
+                        WHERE rowid = NEW.rowid;
+                    END
+                ");
+
+            //Triggers for Song
+            migrationBuilder.Sql(
+                @"
+                    CREATE TRIGGER SetSongTimestampOnUpdate
+                    AFTER UPDATE ON Songs
+                    BEGIN
+                        UPDATE Songs
+                        SET RowVersion = randomblob(8)
+                        WHERE rowid = NEW.rowid;
+                    END
+                ");
+            migrationBuilder.Sql(
+                @"
+                    CREATE TRIGGER SetSongTimestampOnInsert
+                    AFTER INSERT ON Songs
+                    BEGIN
+                        UPDATE Songs
+                        SET RowVersion = randomblob(8)
+                        WHERE rowid = NEW.rowid;
+                    END
+                ");
+            //count performance fee
+            migrationBuilder.Sql(
+                @"
+                    Create View PerformanceFeeSummaries as
+                    Select p.ID, p.FirstName, p.MiddleName, p.LastName, Count(*) as NumberOfPerformances, 
+	                    Avg(a.FeePaid) as AverageFeePaid, Max(a.FeePaid) as MaximumFeePaid, Min(a.FeePaid) as MinimumFeePaid
+                    From Musicians p Join Performances a
+                    on p.ID = a.MusicianID
+                    Group By p.ID, p.FirstName, p.MiddleName, p.LastName
+                ");
+        }
+    }
+}
---
+++ MVC_Music_Solution-Part4/MVC_Music/ViewModels/PerformanceFeeSummaryVM.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace MVC_Music.ViewModels
+{
+    public class PerformanceFeeSummaryVM
+    {
+            public int ID { get; set; }
+
+            [Display(Name = "Musician")]
+            public string FullName
+            {
+                get
+                {
+                    return FirstName
+                        + (string.IsNullOrEmpty(MiddleName) ? " " :
+                            (" " + (char?)MiddleName[0] + ". ").ToUpper())
+                        + LastName;
+                }
+            }
+
+            public string FirstName { get; set; }
+
+            public string MiddleName { get; set; }
+
+            public string LastName { get; set; }
+
+            [Display(Name = "Number of Performances")]
+            public int NumberOfPerformances { get; set; }
+
+            [Display(Name = "Average Fee Paid")]
+            [DataType(DataType.Currency)]
+            public double AverageFeePaid { get; set; }
+
+            [Display(Name = "Maximum Fee Paid")]
+            [DataType(DataType.Currency)]
+            public double MaximumFeePaid { get; set; }
+
+            [Display(Name = "Minimum Fee Charged")]
+            [DataType(DataType.Currency)]
+            public double MinimumFeePaid { get; set; }
+
+    }
+}
